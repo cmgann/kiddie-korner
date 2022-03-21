7b7c7f1fe2a41129d510ed328044fefcdf9aa485
cmgann <casey.m.gann@gmail.com>
2022-03-21 11:18:01 -0500

    removed bootstrap

--- src/app/header/toolbar/toolbar.component.ts
+++ src/app/header/toolbar/toolbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-toolbar',
@@ -7,9 +9,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ToolbarComponent implements OnInit {
 
-  constructor() { }
+  title!: string;
+  url!: string;
+
+  constructor(
+    private router: Router,
+  ) { 
+    router.events.subscribe((val) => {
+      this.urlChange()
+    });
+  }
 
   ngOnInit(): void {
   }
 
-}
+  urlChange() {
+    this.url = this.router.url
+
+
+   if ( this.url == '/') {
+     this.title = 'Kiddie Korner'
+   } else if ( this.url == '/about') {
+     this.title = 'About Us'
+   } else if ( this.url == '/curriculum') {
+     this.title = 'Curriculum'
+   }else if ( this.url == '/meals') {
+    this.title = 'Nutrition'
+  }else if ( this.url == '/schedule') {
+    this.title = 'Schedule'
+  }else if ( this.url == '/parents') {
+    this.title = 'For Parents'
+  }else if ( this.url == '/enrollment') {
+    this.title = 'Enrollment'
+  }else if ( this.url == '/tuition') {
+    this.title = 'Tuition'
+  }else if ( this.url == '/news') {
+    this.title = 'News'
+  }else if ( this.url == '/contact-us') {
+    this.title = 'Contact Us'
+  }   
+ };
+
+};
--- src/app/home/header-cards/header-cards.component.ts
+++ src/app/home/header-cards/header-cards.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AppService } from '../../shared/app.service';
 
 @Component({
   selector: 'app-header-cards',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderCardsComponent implements OnInit {
 
-  constructor() { }
+  isMobile = this.appService.isMobile
+  rowheight: string = '2:1'
+  cols: string = '3'
+
+  constructor(
+    private appService: AppService
+  ) { }
 
   ngOnInit(): void {
+    this.mobileTileAdjust()
   }
 
+  mobileTileAdjust() {
+    if (this.isMobile == true) {
+      console.log('is mobile')
+      this.rowheight = '3:2'
+      this.cols = '1'
+    }
+  }
 }
